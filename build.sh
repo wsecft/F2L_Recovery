8902943b0a9614cd86350a01902c9260bc8cb3a5
u4 <debian.raspi4@gmail.com>
2025-09-24 09:52:44 +0200

    Some better linux integration

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
-sudo apt install -y libxrandr-dev libxcursor-dev libxi-dev libudev-dev
-                                        libfreetype-dev libflac-dev libvorbis-dev
-                                        libgl1-mesa-dev libegl1-mesa-dev libharfbuzz-dev
-mkdir build
+if [ ! -d build ]; then
+    sudo apt update
+    sudo apt install -y \
+        libxrandr-dev libxcursor-dev libxi-dev libudev-dev \
+        libfreetype-dev libflac-dev libvorbis-dev \
+        libgl1-mesa-dev libegl1-mesa-dev libharfbuzz-dev
+fi
 cd build
 cmake ..
 make
+bin/F2L_Recovery
